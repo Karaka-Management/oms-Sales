33ff21365e89c46a6ef174c7550d789fd077b3e7
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-03-24 16:20:24 +0100

    registration fixes

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Sales
  * @copyright Dennis Eichhorn
- * @license   OMS License 1.0
+ * @license   OMS License 2.0
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -24,7 +24,7 @@ use phpOMS\Views\View;
  * Sales class.
  *
  * @package Modules\Sales
- * @license OMS License 1.0
+ * @license OMS License 2.0
  * @link    https://jingga.app
  * @since   1.0.0
  * @codeCoverageIgnore
