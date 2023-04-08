a5861cf98c258c930ed65750fe383fa7ec94432f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-04-08 04:36:26 +0200

    fix billing process

--- Theme/Backend/sales-analysis-dashboard.tpl.php
+++ Theme/Backend/sales-analysis-dashboard.tpl.php
@@ -223,7 +223,7 @@ echo $this->getData('nav')->render();
                             ?>
                                 <tr>
                                     <td><?= $values['month'] . '/' . \substr((string) $values['year'], -2); ?>
-                                    <td><?= (new Money(((int) $values['net_sales']) / 1000))->getCurrency(); ?>
+                                    <td><?= (new Money(((int) $values['net_sales']) / 10000))->getCurrency(); ?>
                                     <td><?= ((int) $values['customers']); ?>
                             <?php endforeach; ?>
                                 <tr>
@@ -343,7 +343,7 @@ echo $this->getData('nav')->render();
                             ?>
                                 <tr>
                                     <td><?= (string) $values['year']; ?>
-                                    <td><?= (new Money(((int) $values['net_sales']) / 1000))->getCurrency(); ?>
+                                    <td><?= (new Money(((int) $values['net_sales']) / 10000))->getCurrency(); ?>
                                     <td><?= ((int) $values['customers']); ?>
                             <?php endforeach; ?>
                     </table>
